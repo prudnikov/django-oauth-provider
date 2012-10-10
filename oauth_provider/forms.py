70b3d352202ee25bf445abbe3bc15df8b69037e5
Vladimir Prudnikov <v.prudnikov@gmail.com>
2012-10-10 20:18:16 +0200

    Add `client_name` to the Authorize form.

--- oauth_provider/forms.py
+++ oauth_provider/forms.py
@@ -1,6 +1,20 @@
 from django import forms
 
+#
+#class AuthorizeRequestTokenForm(forms.Form):
+#    oauth_token = forms.CharField(widget=forms.HiddenInput)
+#    authorize_access = forms.BooleanField(required=False, label='Allow access')
+#
+#
+#
+#from django import forms
+from django.conf import settings
 
 class AuthorizeRequestTokenForm(forms.Form):
     oauth_token = forms.CharField(widget=forms.HiddenInput)
-    authorize_access = forms.BooleanField(required=False, label='Allow access')
+    client_name = forms.CharField(max_length=50,
+        widget=forms.TextInput(attrs={"placeholder":getattr(settings, "OAUTH_PROVIDER_DEFAULT_CLIENT_NAME", ""),"class":"span5"}),
+        help_text="Give this client a name, so later you can differentiate them to revoke access",
+        required=False)
+    authorize_access = forms.BooleanField(required=False, label='Grant access')
+
